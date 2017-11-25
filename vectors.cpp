6ee6a9e93263cfe8200f2af3d5423e6f54c66937
Şerif Geyik <wpram734@gmail.com>
2017-11-26 02:30:52 +0300

    vectors
    vectors example

---
+++ vectors.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> v;
+    for (int i=0;i<n;i++)
+    {
+        int input=0;
+        cin >>input;
+        v.push_back(input);
+        
+    }
+    sort(v.begin(),v.end());
+    
+    for(int i=0;i<v.size();i++)
+    {
+        
+        cout << v[i] << " ";
+        
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
